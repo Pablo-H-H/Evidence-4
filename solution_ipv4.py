ba04598f402e117ff9f1c25dd311b3d017ae5cd2
Pablo H <111140061+Pablo-H-H@users.noreply.github.com>
2024-06-06 07:29:28 -0600

    Update solution_ipv4.py

--- solution_ipv4.py
+++ solution_ipv4.py
@@ -18,9 +18,5 @@ class Solution:
 # Example usage:
 solution = Solution()
 print(solution.validIPAddress("192.168.1.1"))         # "IPv4"
-print(solution.validIPAddress("192.168.01.1"))        # "Neither"
-print(solution.validIPAddress("192.168.1.0"))         # "IPv4"
 print(solution.validIPAddress("2001:0db8:85a3:0000:0000:8a2e:0370:7334")) # "IPv6"
-print(solution.validIPAddress("2001:db8:85a3:0:0:8A2E:0370:7334"))        # "IPv6"
 print(solution.validIPAddress("2001:0db8:85a3:0000:0000:8a2e:0370:7334:")) # "Neither"
-print(solution.validIPAddress("2001:0db8:85a3:0:0:8a2e:0370:7334:1234"))   # "Neither"
